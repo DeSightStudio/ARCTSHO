d79af399895d4e74f9a29c54a72e4a4cb04470d0
Dominik Waitzer <dominik.waitzer@desightstudio.com>
2024-10-15 15:26:27 +0200

    Include FontAwesome and some JS

--- dev/js/dist.js
+++ dev/js/dist.js
@@ -1,6 +1,9 @@
 (function($) {
     'use strict';
 
+    // Global variables
+    var $window = $(window);
+
     setTimeout(function() {
 
         // Function to check if the element exists and then call a callback
@@ -13,9 +16,53 @@
             }, 100);
         }
 
+        // Custom function to toggle visibility of elements
+        function toggleVisibility(selector) {
+            $(selector).toggle();
+        }
+
+        // Custom function to debounce events
+        function debounce(func, wait) {
+            let timeout;
+            return function executedFunction(...args) {
+                const later = () => {
+                    clearTimeout(timeout);
+                    func(...args);
+                };
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+            };
+        }
+
+        // Custom function to smooth scroll to an element
+        function smoothScroll(target) {
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 600);
+        }
+
         // Initialization
         function initialize() {
-            
+            // Example usage of custom functions
+            onElementReady('.my-element', function() {
+                console.log('Element is ready!');
+            });
+
+            // Event listener for a button to toggle visibility
+            $('#toggle-button').on('click', function() {
+                toggleVisibility('.toggle-target');
+            });
+
+            // Debounced resize event
+            $(window).on('resize', debounce(function() {
+                console.log('Window resized!');
+            }, 250));
+
+            // Smooth scroll on link click
+            $('a.smooth-scroll').on('click', function(e) {
+                e.preventDefault();
+                smoothScroll($(this).attr('href'));
+            });
         }
 
         // Document ready
